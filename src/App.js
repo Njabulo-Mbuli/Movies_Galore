30af4586a2b7af6e14fa2714cc89178a6b301374
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-06-28 11:55:37 +0200

    fetching data with promise all

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Jumbatron from './Components/Jumbatron/Jumbatron'
 import './App.css';
+
+const api_key='c775303404fc7d314a5190e0708c61bf';
 
 class App extends Component{
   constructor(){
     super();
 
+    Promise.all([
+        fetch(`https://api.themoviedb.org/3/movie/latest?api_key=${api_key}&language=en-US`),
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`),
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+      ]).then(([latest,top_rated,popular])=>{
+        console.log("latest: ",latest);
+        console.log("top_rated: ",top_rated.json());
+        console.log("popular: ",popular.json());
+      });
+    
+
+    this.state={
+      name:''
+    }
   }
   render(){
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Jumbatron/>
     </div>
   );
 }
---
+++ src/Components/Jumbatron/Jumbatron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Jumbatron = () =>{
+	return(
+		<div className="jumbatron" style={{background:'rgba(45,12,45,1)',width:'100%',height:'100%'}}>
+			
+			<h1>Hello from Jumbatron</h1>
+
+		</div>
+		);
+}
+
+export default Jumbatron;
