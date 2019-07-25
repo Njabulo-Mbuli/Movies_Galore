6b6a7d44133d2c08d8ca22083c866c6fe7822885
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-07-25 13:10:18 +0200

    Fixed small bug on sidedrawer backdrop

--- src/Components/BackDrop/Backdrop.js
+++ src/Components/BackDrop/Backdrop.js
@@ -4,7 +4,7 @@ import './Backdrop.css';
 const Backdrop = (props) =>{
 	return(props.show?
 	<div className="Backdrop" onClick={props.hideModal}></div>:
-	<div className="Backdrop" style={{transform:'translateY(-100vh)',opacity:'0'}} 
+	<div className="Backdrop" style={{transform:'translateY(-150vh)',opacity:'0'}} 
 	onClick={props.hideModal}></div>);
 }
 
