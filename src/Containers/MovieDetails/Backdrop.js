4f5ae4e4e0690ce51134af2716726510133f0da0
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-08-10 20:52:26 +0200

    Undid the change

--- src/Containers/MovieDetails/Backdrop.js
+++ src/Containers/MovieDetails/Backdrop.js
@@ -5,7 +5,7 @@ const Backdrop =(props)=>{
 	let backup_background=require("../../assets/no_image_found.png");
 	
 	return(
-		<div className="Back_Drop" style={{marginTop:"3.2em",backgroundImage:`url(https://image.tmdb.org/t/p/original/${props.backdrop_path}),url(${backup_background}`}}>
+		<div className="Back_Drop" style={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${props.backdrop_path}),url(${backup_background}`}}>
 			<div style={{margin:"0",padding:"1em",textAlign:"center"}}>{props.tagline?<h4 style={{textShadow: "2px 2px 3px #000000"}}>{props.tagline}</h4>:null}</div>
 		</div>
 	);
